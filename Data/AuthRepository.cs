255ee71a83e9fb93319465f3e3cf7665b3b670b1
franekfranek <franciszekezawadzki@gmail.com>
2020-04-16 22:14:03 +0200

    IAuthRepository interface created/AuthRepository concrete class created-> register method added

---
+++ Data/AuthRepository.cs
@@ -0,0 +1,53 @@
+﻿using DatingApp.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _context;
+
+        public AuthRepository(DataContext context)
+        {
+            this._context = context;
+        }
+        public Task<User> Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User> Register(User user, string password)
+        {
+            byte[] passwordHash, passwordSalt; //we initialize them in the method above thanks to out key words
+            CreatePasswordHash(password, out passwordHash, out passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            //hmac implements IDisposable means can use Dispose() method means we can use using statement
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+            
+        }
+
+        public Task<bool> UserExist(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
