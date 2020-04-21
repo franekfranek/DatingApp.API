d2c47f524ef0496af3eb48cce8455153d324e162
franekfranek <franciszekezawadzki@gmail.com>
2020-04-21 22:34:30 +0200

    User DTO validation added

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -23,6 +23,7 @@ namespace DatingApp.API.Controllers
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserForRegisterDto user) //[FromBody] can be used here but its not necesarry cuase of[ApiController]
         {
+            //also (!ModelState.IsValid is not necesarry here because ApiController supply it)
             user.Username = user.Username.ToLower();
 
             if(await _repo.UserExist(user.Username)) 
--- DTOSs/UserForRegisterDto.cs
+++ DTOSs/UserForRegisterDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,12 @@ namespace DatingApp.API.DTOSs
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = 
+            "Password must be at least 4 characters!")]
         public string Password { get; set; }
 
     }
