b70e389f9580cd51cb9f754a93d4eb7b560f269e
franekfranek <franciszekezawadzki@gmail.com>
2020-05-15 22:13:38 +0200

    random data from json generator seeded to the db

---
+++ Data/Seed.cs
@@ -0,0 +1,48 @@
+﻿using DatingApp.API.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Data
+{
+    public class Seed
+    {
+        public static void SeedUser(DataContext context)
+        {
+            if (!context.Users.Any())
+            {
+                var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
+
+                var users = JsonConvert.DeserializeObject<List<User>>(userData);
+
+                foreach (var user in users)
+                {
+                    byte[] passwordHash, passworSalt;
+                    CreatePasswordHash("password", out passwordHash, out passworSalt); //out means same obj not copy
+
+                    user.PasswordHash = passwordHash;
+                    user.PasswordSalt = passworSalt;
+                    user.Username = user.Username.ToLower();
+                    context.Users.Add(user);
+                }
+
+                context.SaveChanges();
+
+                //this dont have to be async method because we do it only once before any users so no concurency can appear==
+                //no other method will be called when this called
+            }
+        }
+        private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            //hmac implements IDisposable means can use Dispose() method means we can use using statement
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using DatingApp.API.Data;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System;
 
 namespace DatingApp.API
 {
@@ -13,7 +12,25 @@ namespace DatingApp.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using(var scope = host.Services.CreateScope())
+            {
+                var serivces = scope.ServiceProvider;
+                try
+                {
+                    var context = serivces.GetRequiredService<DataContext>();
+                    context.Database.Migrate(); //it will check every time we run application if there is a pending migration
+                    //if yes it will create it for us
+                    Seed.SeedUser(context);
+                    //this is usedful because if we mess up db we can just drop it and start again with a clean one
+                }
+                catch (Exception ex)
+                {
+                    var logger = serivces.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error accured during migration");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
